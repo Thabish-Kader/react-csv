e30cf43cb9fae334097ac6b89ea65e0c56c727fd
Thabish <kadertabish2@gmail.com>
2023-08-27 10:07:48 +0530

    readme
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,163 @@
-import Image from 'next/image'
+"use client";
+import React, { CSSProperties, useState, useEffect } from "react";
+import { useCSVReader } from "react-papaparse";
+import { BsCloudUploadFill } from "react-icons/bs";
+import { GiCancel } from "react-icons/gi";
+const styles = {
+	csvReader: {
+		display: "flex",
+		flexDirection: "row",
+		marginBottom: 10,
+	} as CSSProperties,
+	browseFile: {
+		width: "20%",
+	} as CSSProperties,
+	acceptedFile: {
+		border: "1px solid #ccc",
+		height: 45,
+		lineHeight: 2.5,
+		paddingLeft: 10,
+		width: "80%",
+	} as CSSProperties,
+	remove: {
+		borderRadius: 0,
+		padding: "0 20px",
+	} as CSSProperties,
+	progressBarBackgroundColor: {
+		backgroundColor: "green",
+	} as CSSProperties,
+};
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+export default function CSVReader() {
+	const { CSVReader } = useCSVReader();
+	const [csvData, setCsvData] = useState<any>();
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	function transformCSVtoJSON(csvData: any[][]): any[] | null {
+		if (!csvData || csvData.length === 0) {
+			return null; // Return null or handle empty data case
+		}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		const [headers, ...rows] = csvData; // Destructure the first row as headers and rest as rows
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		const jsonData = rows.map((row) => {
+			const obj: { [key: string]: any } = {};
+			headers.forEach((header, index) => {
+				obj[header] = row[index];
+			});
+			return obj;
+		});
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+		return jsonData;
+	}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	const jsonFormattedData = transformCSVtoJSON(csvData);
+	console.log(jsonFormattedData);
+
+	return (
+		<div className="max-w-5xl mx-auto px-24 pt-20">
+			<div className="flex items-center tracking-widest cursor-pointer">
+				<p className="text-blue-500">This is CSV Upload Vid.</p>
+			</div>
+			<h1 className="mt-4 text-4xl font-bold">Upload CSV</h1>
+			<CSVReader
+				onUploadAccepted={({ data }: { data: any }) => {
+					setCsvData(data);
+				}}
+			>
+				{({
+					getRootProps,
+					acceptedFile,
+					ProgressBar,
+					getRemoveFileProps,
+				}: any) => (
+					<>
+						<div className="mt-4 relative" style={styles.csvReader}>
+							<button
+								type="button"
+								{...getRootProps()}
+								className="relative h-[300px] flex flex-col justify-center items-center w-full border border-dashed border-gray-800 rounded-md"
+							>
+								{acceptedFile ? (
+									<p className="font-bold text-xl">
+										{acceptedFile.name}
+									</p>
+								) : (
+									<BsCloudUploadFill className="h-52 w-52 text-green-500" />
+								)}
+							</button>
+							<button
+								{...getRemoveFileProps()}
+								className="absolute top-2 right-2"
+							>
+								<GiCancel className="text-red-500" siz={30} />
+							</button>
+							<ProgressBar
+								className="absolute bottom-4"
+								style={styles.progressBarBackgroundColor}
+							/>
+						</div>
+					</>
+				)}
+			</CSVReader>
+
+			{csvData?.length > 0 && (
+				<div className="max-w-7xl mx-auto px-24 pt-20">
+					<table className="w-full">
+						<tr className="">
+							<th>Column Name</th>
+							<th>Select Type</th>
+							<th>Samples</th>
+						</tr>
+					</table>
+					<tbody className="w-full">
+						{csvData[0]?.map(
+							(columnName: string, columnIndex: number) => (
+								<tr
+									key={columnIndex}
+									className="flex justify-between border-b py-4"
+								>
+									<td className="w-56">{columnName}</td>
+									<td className="mx-10 w-56">
+										<select className="flex-1 bg-transparent border rounded-md px-4 py-1">
+											{csvData[0].map(
+												(
+													optionName: string,
+													optionIndex: number
+												) => (
+													<option
+														key={optionIndex}
+														value={optionName}
+														selected={
+															optionName ===
+															columnName
+														}
+													>
+														{optionName}
+													</option>
+												)
+											)}
+										</select>
+									</td>
+									<td className="w-56 flex flex-col font-bold">
+										{csvData
+											.slice(1, 5)
+											.map(
+												(
+													entry: string,
+													entryIndex: number
+												) => (
+													<p key={entryIndex}>
+														{entry[columnIndex]}
+													</p>
+												)
+											)}
+									</td>
+								</tr>
+							)
+						)}
+					</tbody>
+				</div>
+			)}
+		</div>
+	);
 }
